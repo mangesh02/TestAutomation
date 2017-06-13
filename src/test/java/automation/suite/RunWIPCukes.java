b0085225a4b963e3b46f228f287a3d1f1e2251b4
Mangesh Shinde <mangesh.shinde@noexternalmail.hsbc.co.in>
2017-06-13 14:03:30 +0530

    Changes for testing jenkins

--- src/test/java/automation/suite/RunWIPCukes.java
+++ src/test/java/automation/suite/RunWIPCukes.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(plugin = { "pretty", "json:target/cucumber.json" }, glue = { "automation.stepdefs",
-		"browsers" }, features = "src/test/resources/features", monochrome = true, tags = {"~@androidbrowser","@androidapp"})
+		"browsers" }, features = "src/test/resources/features", monochrome = true, tags = {"@androidbrowser","~@androidapp"})
 public class RunWIPCukes {
 
 }
